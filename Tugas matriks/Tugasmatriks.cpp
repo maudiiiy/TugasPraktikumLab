cfe6b789f77fced81e0292683afd18e077846dc7
maudiiiy <156888963+maudiiiy@users.noreply.github.com>
2024-03-25 09:09:48 +0700

    Add files via upload
    Tugas Matriks

---
+++ Tugas matriks/Tugasmatriks.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int b[5] = {2,5,3,2,4};
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "nilai dari indeks-i:" <<i<< ":" << b[i] << std::endl;
+        }
+
+    return 0;
+}
